52b6d38ddce4906045487b88d5583c2ae6ce2f8a
lokesh.k.anand@gmail.com <lokesh.k.anand@gmail.com>
2018-04-08 23:08:17 -0500

    added note selectors

--- client/src/App.js
+++ client/src/App.js
@@ -1,118 +1,114 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import NoteSelector from "./components/NoteSelector";
 import WTWrapper from "./components/WTWrapper";
 import Wrapper from "./components/Wrapper";
 
+const notes = ["sixtyfourth", "thirtysecond", "sixteenth", "eighth", "quarter", "half", "whole"];
+const spaces = [1, 2, 4, 8, 16, 32, 64];
+
 class App extends Component {
-state={
-  allNotes:[],
-	measureNumber:1
-}
-
-componentDidMount(){
-  this.addMeasure();
-  return false;
-}
-
-addMeasure=()=>{
-
-  let tempArr=this.state.allNotes;
-  
-
-  let mArray=[];
-  for(let j=1;j<5;j++){
-    let bArray=[];
-    for(let k=1;k<7;k++){
-      let lArray=[];
-      for(let l=1;l<17;l++){
-        const noteObject = {
-          snoteID: "m"+this.state.measureNumber+"-b"+j+"-l"+k+"-s"+l,
-          value: "",
-          clicked: false,
-          noteEntered: ""
-        };
-        lArray.push(noteObject);
-      }
-      bArray.push(lArray);
-    }
-    mArray.push(bArray);
+  state={
+    allNotes:[],
+  	measureNumber:1,
+    noteSelected: "",
+    noteType: ""
   }
-  tempArr.push(mArray);
-  let counter=this.state.measureNumber++;
-  this.setState({allNotes:tempArr,measureNumber:counter});
-}
 
-//i click on id and it changes from short black div to text box
+  componentDidMount(){
+    this.addMeasure();
+    return false;
+  }
 
+  addMeasure=()=>{
 
-// handleInputChange = event => {
+    let tempArr=this.state.allNotes;
     
-//     const { name, value } = event.target;
-//     this.setState({
-//       [name]: value
-//     });
-//     if (event.keyCode === 10 || event.keyCode === 13) {
-//       handleFormSubmit();
-//     }
-//   };
-extractId = (id) => {
-  let idArray = id.thisid.split("-");
-  let note = {
-    measure: idArray[0].substring(1),
-    beat: idArray[1].substring(1),
-    line: idArray[2].substring(1),
-    sNote: idArray[3].substring(1)
-  };
-  return note;
-}
 
-noteClick=(event,id)=>{
-  event.preventDefault();
-  let note = this.extractId(id);
-  let allNotesCopy = this.state.allNotes;
-  allNotesCopy[note.measure][note.beat][note.line][note.sNote].clicked = true;
-  this.setState({allNotes: allNotesCopy});
-}
-
-noteSubmit = (event,id)=>{
-  event.preventDefault();
-  let note = this.extractId(id);
-  let measure = note.measure;
-  let beat = note.beat;
-  let line = note.line;
-  let sNote = note.sNote;
-  let allNotesCopy = this.state.allNotes;
-  allNotesCopy[measure][beat][line][sNote].clicked = false;
-  let noteEntered2 = allNotesCopy[measure][beat][line][sNote].noteEntered;
-  let parseNote=parseFloat(noteEntered2);
-  if(noteEntered2 === "")
-    allNotesCopy[measure][beat][line][sNote].value = "";
-  else if(isNaN(noteEntered2) || parseNote % 1 !== 0 || parseNote<1||parseNote>24){
-    allNotesCopy[measure][beat][line][sNote].value = "";
-    allNotesCopy[measure][beat][line][sNote].noteEntered = "";
+    let mArray=[];
+    for(let j=1;j<5;j++){
+      let bArray=[];
+      for(let k=1;k<7;k++){
+        let lArray=[];
+        for(let l=1;l<17;l++){
+          const noteObject = {
+            snoteID: "m"+this.state.measureNumber+"-b"+j+"-l"+k+"-s"+l,
+            value: "",
+            clicked: false,
+            noteEntered: ""
+          };
+          lArray.push(noteObject);
+        }
+        bArray.push(lArray);
+      }
+      mArray.push(bArray);
+    }
+    tempArr.push(mArray);
+    let counter=this.state.measureNumber++;
+    this.setState({allNotes:tempArr,measureNumber:counter});
   }
 
-  else {
-    allNotesCopy[measure][beat][line][sNote].value = parseNote;
-    allNotesCopy[measure][beat][line][sNote].noteEntered = "";
+  extractId = (id) => {
+    let idArray = id.thisId.split("-");
+    let note = {
+      measure: idArray[0].substring(1),
+      beat: idArray[1].substring(1),
+      line: idArray[2].substring(1),
+      sNote: idArray[3].substring(1)
+    };
+    return note;
   }
-  this.setState({allNotes: allNotesCopy});
-}
 
-noteChange = (event,id) => {
-  event.preventDefault();
-  let note = this.extractId(id);
-  let allNotesCopy = this.state.allNotes;
-  allNotesCopy[note.measure][note.beat][note.line][note.sNote].noteEntered = event.target.value;
-  this.setState({allNotes: allNotesCopy});
-}
+  noteClick=(event,id)=>{
+    event.preventDefault();
+    let note = this.extractId(id);
+    let allNotesCopy = this.state.allNotes;
+    allNotesCopy[note.measure][note.beat][note.line][note.sNote].clicked = true;
+    this.setState({allNotes: allNotesCopy});
+  }
 
+  noteSubmit = (event,id)=>{
+    event.preventDefault();
+    let note = this.extractId(id);
+    let measure = note.measure;
+    let beat = note.beat;
+    let line = note.line;
+    let sNote = note.sNote;
+    let allNotesCopy = this.state.allNotes;
+    allNotesCopy[measure][beat][line][sNote].clicked = false;
+    let noteEntered2 = allNotesCopy[measure][beat][line][sNote].noteEntered;
+    let parseNote=parseFloat(noteEntered2);
+    if(noteEntered2 === "")
+      allNotesCopy[measure][beat][line][sNote].value = "";
+    else if(isNaN(noteEntered2) || parseNote % 1 !== 0 || parseNote<1||parseNote>24){
+      allNotesCopy[measure][beat][line][sNote].value = "";
+      allNotesCopy[measure][beat][line][sNote].noteEntered = "";
+    }
+
+    else {
+      allNotesCopy[measure][beat][line][sNote].value = parseNote;
+    }
+    this.setState({allNotes: allNotesCopy});
+  }
+
+  noteChange = (event,id) => {
+    event.preventDefault();
+    let note = this.extractId(id);
+    let allNotesCopy = this.state.allNotes;
+    allNotesCopy[note.measure][note.beat][note.line][note.sNote].noteEntered = event.target.value;
+    this.setState({allNotes: allNotesCopy});
+  }
+
+  setNoteType = (noteType) => {
+    this.setState({noteType: noteType});
+  }
 
   render() {
     return (
       <Wrapper>
-        <h1>Enter any fret from 1 to 24</h1>
-      	<button onClick={this.addMeasure}>Test</button>
+        <h1>Select a note and then enter any fret from 1 to 24</h1>
+        <NoteSelector notes = {notes} selectedNoteType = {this.state.noteType} setNoteType = {this.setNoteType}/>
+      	<button onClick={this.addMeasure}>Add Measure</button>
       	<WTWrapper allNotes={this.state.allNotes} noteClick={this.noteClick}
         noteSubmit={this.noteSubmit} noteChange = {this.noteChange}/>
       </Wrapper>
--- client/src/Components/Snote/Snote.js
+++ client/src/Components/Snote/Snote.js
@@ -3,18 +3,18 @@ import "./Snote.css";
 
 const Snote = props => {
 	return props.arrayOfNotes.map((element, index)=>{
-		let thisid=props.lineIndex+'-s'+index;
+		let thisId=props.lineIndex+'-s'+index;
 		let elementToReturn;
 		if(element.clicked === false&&element.value==="")
 			elementToReturn = <div className = "editableNote"></div>;
 		else if (element.clicked === false&&element.value!=="")
 			elementToReturn=<div className="enteredNote">{element.value}</div>;
 		else if (element.clicked===true&&element.value==="")
-			elementToReturn = <form onSubmit={(e)=>e.preventDefault()} className = "newNote"> <input type = "text" onBlur={(e) => props.noteSubmit(e,{thisid})} onChange={(e)=>props.noteChange(e,{thisid})}/> </form>;
+			elementToReturn = <form onSubmit={(e)=>props.noteSubmit(e, {thisId})} className = "newNote"> <input type = "text" onBlur={(e) => props.noteSubmit(e,{thisId})} onChange={(e)=>props.noteChange(e,{thisId})}/> </form>;
 		else
-			elementToReturn = <form onSubmit={(e)=>e.preventDefault()} className = "noteForm"> <input defaultValue={element.value} type = "text" onBlur={(e) => props.noteSubmit(e,{thisid})} onChange={(e)=>props.noteChange(e,{thisid})}/> </form>;
+			elementToReturn = <form onSubmit={(e)=>props.noteSubmit(e, {thisId})} className = "noteForm"> <input defaultValue={element.value} type = "text" onBlur={(e) => props.noteSubmit(e,{thisId})} onChange={(e)=>props.noteChange(e,{thisId})}/> </form>;
 		return (
-			<div className="note" key={index} id={props.lineIndex+'-s'+index} onClick={(e) => props.noteClick(e,{thisid})}>
+			<div className="note" key={index} id={props.lineIndex+'-s'+index} onClick={(e) => props.noteClick(e,{thisId})}>
 				{elementToReturn}
 			</div>
 		);
