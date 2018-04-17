bf4fe7ff22f482390cf4fcb1f25411c6939ccd03
kaybutswag <kbwagner426@gmail.com>
2018-04-17 09:44:11 -0500

    login page working with database

--- passport.js
+++ passport.js
@@ -1,9 +1,32 @@
 const session = require('express-session');
+const cookieparser = require('cookie-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const db = require('./models');
 
 module.exports = (app) => {
+
+    app.use(cookieparser());
+  app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    // automatically extends the session age on each request. useful if you want
+    // the user's activity to extend their session. If you want an absolute session
+    // expiration, set to false
+    rolling: true,
+    name: 'sid', // don't use the default session cookie name
+    // set your options for the session cookie
+    cookie: {
+      httpOnly: true,
+      // the duration in milliseconds that the cookie is valid
+      maxAge: 20 * 60 * 1000, // 20 minutes
+      // recommended you use this setting in production if you have a well-known domain you want to restrict the cookies to.
+      // domain: 'your.domain.com',
+      // recommended you use this setting in production if your site is published using HTTPS
+      // secure: true,
+    }
+  }));
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
