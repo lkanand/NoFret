eda9e1fad3c23c5552ca7d1e289da77800e8519a
PMMIV <pmmilamiv@gmail.com>
2018-04-18 15:02:45 -0500

    cleaned up printing styles

--- client/src/Components/Snote/Snote.js
+++ client/src/Components/Snote/Snote.js
@@ -3,22 +3,17 @@ import "./Snote.css";
 
 class Snote extends Component {
 
-	componentDidMount(props) {
-		console.log("====mounted====");
-		console.log(this.props);
-	}
-
-	componentWillReceiveProps(props) {
-		console.log("recievedProps");
-		console.log(this.props);
-	}
-
 	focus(input) {
 		if(input !== null && this.props.activeNoteId === "") {
 			input.focus();
 			input.select();
 			this.props.setActiveNote(input.parentElement.parentElement.id);
 		}
+	}
+
+	getNote(element) {
+		console.log("========")
+		console.log(element);
 	}
 
 	blurInput(event, thisId) {
@@ -32,7 +27,7 @@ class Snote extends Component {
 			if(element.disabled===true){
 				return (
 				<div className="disabledNoteBox" key={index} id={this.props.lineIndex+'-s'+index}>
-					<div className="disabledNote Db">{/*note*/}</div>
+					<div className="disabledNote">{/*note*/}</div>
 				</div>
 				);
 				
@@ -41,9 +36,9 @@ class Snote extends Component {
 				if(element.clicked === false&&element.value==="")
 					elementToReturn = <div className = "editableNote"></div>;
 				else if (element.clicked === false&&element.value!=="")
-					elementToReturn=<div className="enteredNote Db">{/*note*/}{element.value}</div>;
+					elementToReturn=<div className="enteredNote">{/*note*/}{element.value}</div>;
 				else
-					elementToReturn = <form onSubmit={(e)=>this.props.noteSubmit(e, {thisId})} className = "noteForm" onBlur={(e) => this.blurInput(e,{thisId})} > 
+					elementToReturn = <form onSubmit={(e, element)=>{this.props.noteSubmit(e, {thisId}); this.getNote(element);}} className = "noteForm" onBlur={(e) => this.blurInput(e,{thisId})} > 
 										<input defaultValue = {element.value} ref = {(input) => {this.focus(input)}} onChange={(e)=>this.props.noteChange(e,{thisId})}
 										onKeyDown = {(e) => this.props.incOrDecDuration(e, {thisId})} /> 
 									</form>;
